4c0d0b5455942e16cda3933c16d24d667df75433
Joe <reed.joseph13@gmail.com>
2019-09-05 17:07:10 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SummaryComponent } from './summary/summary.component';
+import { ActivitiesComponent } from './activities/activities.component';
+import { PaymentsComponent } from './payments/payments.component';
+import { OffersComponent } from './offers/offers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SummaryComponent,
+    ActivitiesComponent,
+    PaymentsComponent,
+    OffersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
